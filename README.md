10ff7fa43b7bee257267d21875a0b97fb1dd100a
Bernardo Sozo Fattini <1134300@atitus.edu.br>
2023-06-21 19:43:02 -0300

    commit 1

---
+++ README.md
@@ -0,0 +1,6 @@
+Space Discovery
+
+Projeto desenvolcido pelos alunos: 
+
+Gabriel Pradegan Orsatto - RA: 1135097
+Bernardo Sozo Fattini - RA: 1134300
---
+++ geraSetup.py
@@ -0,0 +1,18 @@
+#pip install cx_freeze
+import cx_Freeze
+executables =[
+    cx_Freeze.Executable(script="main.py", icon="icone.ico")
+]
+cx_Freeze.setup(
+    name = "Space Discovery",
+    options={
+        "build_exe":{
+            "packages":["pygame"],
+            "include_files":["bg.jpg",
+                            "musica.mp3"]
+        }
+    }, executables = executables
+)
+
+#py setup.py build
+#py setup.py bdist_msi
