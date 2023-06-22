a2759c61b3854326adfb0c33490ade7d23e85887
Bernardo Sozo Fattini <1134300@atitus.edu.br>
2023-06-22 15:47:42 -0300

    commit2

--- main.py
+++ main.py
@@ -3,35 +3,43 @@ import pygame
 #inicializar o pygame
 pygame.init()
 tamanho = (1000,563)
-
-clock = pygame.time.Clock()
+branco = (255, 255, 255)
 tela = pygame.display.set_mode( tamanho )
+
 icone = pygame.image.load("icone.ico")
 pygame.display.set_icon( icone )
 fundo = pygame.image.load("bg.jpg")
 pygame.display.set_caption("Space Discovery")
 pygame.mixer.music.load("musica.mp3")
 pygame.mixer.music.play(-1)
+clock = pygame.time.Clock()
 
 space = pygame.image.load("space.png")
 
 running = True
+
+circulos = []
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False    
-
-        
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                circulos.append(event.pos)
 
     #aqui vai o código em si
     tela.blit(fundo, (0,0) )
     tela.blit(space, (50,30) )
 
+    for posicao in circulos:
+        pygame.draw.circle(tela, branco, posicao, 5)
+
 
     pygame.display.update()
     clock.tick(40)
+
     
 pygame.quit()
