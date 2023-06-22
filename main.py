7188cc0b2fdf365ab9966a66c5e435e92761a022
Bernardo Sozo Fattini <1134300@atitus.edu.br>
2023-06-22 16:22:05 -0300

    commit10

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import pygame
 import json
 from tkinter import simpledialog
 
-#inicializar o pygame
 pygame.init()
+
 tamanho = (1000,563)
-branco = (255, 255, 255)
 tela = pygame.display.set_mode( tamanho )
 
 icone = pygame.image.load("icone.ico")
 pygame.display.set_icon( icone )
+
 fundo = pygame.image.load("bg.jpg")
+
 pygame.display.set_caption("Space Discovery")
+
 pygame.mixer.music.load("musica.mp3")
 pygame.mixer.music.play(-1)
+
 clock = pygame.time.Clock()
+
 fonte = pygame.font.SysFont(None, 20)
+
+branco = (255, 255, 255)
 F10 = fonte.render('Pressione F10 para salvar os pontos', True, branco)
 F11 = fonte.render('Pressione F11 para carregar os pontos', True, branco)
 F12 = fonte.render('Pressione F12 para deletar os pontos', True, branco)
@@ -30,20 +36,24 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
+
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F10:
             try:
                 with open('db.txt','w') as database:
                     json.dump(estrelas,database)
             except:
                 print("Erro ao criar base de dados")
+
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
             try:
                 with open('db.txt') as database:
                     estrelas = json.load(database)
             except:
                 print("Erro ao salvar os pontos")
+
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F12:
             estrelas.clear()
+
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             posicaoEstrela = pygame.mouse.get_pos()
             nomeEstrela = simpledialog.askstring("Space", "Nome da Estrela: ")
@@ -51,13 +61,11 @@ while running:
                 if nomeEstrela.strip() == "":
                     nomeEstrela = "Desconhecido"+str(posicaoEstrela)      
                 estrelas[nomeEstrela] = posicaoEstrela
-                print(estrelas)
                
-    tela.blit(fundo, (0,0) )
+    tela.blit(fundo, (0,0))
     tela.blit(F10, (690, 480))
     tela.blit(F11, (690, 500))
     tela.blit(F12, (690, 520))
-
 
     for nomeEstrela, posicaoEstrela in estrelas.items():
         try:
@@ -67,6 +75,7 @@ while running:
                 tela.blit(texto, posicaoEstrela)
         except:
             print("Erro 273627x02. Reinicie a aplicação!")
+
         if len(estrelas) >= 2:
             pontos = list(estrelas.values())
             pygame.draw.lines(tela, branco, False, pontos, 1)
